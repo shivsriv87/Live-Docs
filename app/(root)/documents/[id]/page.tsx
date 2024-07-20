b8fe375fae58a3c1c1d4d7c022b1041a3bf4326f
Shivang Srivastava <shiv.sriv87@gmail.com>
2024-07-20 15:18:56 +0530

    docuement editor page without authentication

---
+++ app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Editor } from '@/components/editor/Editor'
+import Header from '@/components/Header'
+
+const Document = () => {
+  return (
+    <div>
+      <Header>
+        <div className='flex w-fit items-center justify-center gap-2' >
+          <p className='document-title'>this is a fake document title</p>
+        </div>
+      </Header>
+      <Editor/>
+
+
+    </div>
+  )
+}
+
+export default Document
