bc282955548afa3db69a8171f8f2bfac165f4fdd
Shivang Srivastava <shiv.sriv87@gmail.com>
2024-07-23 21:37:38 +0530

    homepage done

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,11 +1,13 @@
+import CollaborativeRoom from '@/components/CollaborativeRoom'
 import React from 'react'
 
 
 const Document = () => {
   return (
-    <div>
-      
-    </div>
+    <main className=' flex flex-col items-center'>
+      <CollaborativeRoom
+      />
+    </main>
   )
 }
 
